f4c6edadb499300d929332286085cf088b85a00e
Dean151 <thomas.durand.verjat@gmail.com>
2015-03-05 08:51:17 +0100

    Creating empty grid, Adding Show Instance to Cell

--- connectFour.hs
+++ connectFour.hs
@@ -1,12 +1,22 @@
+------------------------------------------------------------
 -- Connect Four Game in Haskell Programming Language
 -- Created by Thomas Durand - 2015/03/05
+------------------------------------------------------------
 
 -- Creating the two colors for the players
-data Color = Red | Yellow
+data Color = Red | Yellow deriving (Show)
+
 -- A cell can be empty, or filled with one of the colors
 data Cell = Empty | Filled Color
-
+instance Show Cell where 
+	show Empty 			 = "   "
+	show (Filled Red) 	 = " R "
+	show (Filled Yellow) = " Y "
+ 
 -- Creating aliases types for Columns and Grid
 type Column = [Cell]
 type Grid = [Column]
 
+-- Creating the starting empty Grid
+emptyColumn = replicate 6 Empty
+grid = replicate 7 emptyColumn
